415bd89243f53a0cfc6305c5166957be20858c44
Scalpi <sergey6661313@gmail.com>
2021-11-12 12:14:30 +0300

    just add a plan to work

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# ScalpiEditor
-
-depends: zig
+# ScalpiEditor is "hierarchy" text editor for programmers and others...
 
 for compile:
 $ zig build
+
+current stage of development*: 
+    [ ] files are read
+    [ ] editor allows to make changes
+    [ ] files are saved
+    
+[ ] - means in development 
+
+depends: 
+    zig v0.9
+
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
 
+const Modes = enum {
+    navigation,
+    edit,
+    command,
+}
+
 pub fn main() anyerror!void {
-    std.log.info("Hello World!", .{});
+    std.log.info("{s}:{}: Hello!", .{@src().file, @src().line});
+    // TODO create buffer screen
+    // TODO save file
+    std.log.info("{s}:{}: Bye!", .{@src().file, @src().line});
 }
