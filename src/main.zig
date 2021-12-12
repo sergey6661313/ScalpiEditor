f694a1c565717372f2697561ba56accd71bf295a
Scalpi <sergey6661313@gmail.com>
2021-12-12 17:30:29 +0000

    moved load file to one func

--- src/main.zig
+++ src/main.zig
@@ -29,7 +29,7 @@ file_name:   [1024]u8         = undefined,
 keyboard:    Keyboard         = .{},
 lines:       [lines_max]Line  = .{.{}}**lines_max, // OwO syntax
 lines_count: usize            = 0,
-lines_index: [lines_max]u16   = undefined,
+lines_index: [lines_max]usize = undefined,
 // zig fmt: on
 
 pub var prog: Prog = .{};
@@ -101,8 +101,8 @@ pub fn main() error{
         var pos: usize = 0;
         while(true) {
             self.lines_index[pos] = pos;
-            if (pos == lines_max) break;
             pos += 1;
+            if (pos == lines_max) break;
         }
     }
 
@@ -125,46 +125,23 @@ pub fn main() error{
         var argIterator = &argIterator_packed.inner;
         _ = argIterator.skip();
         while (argIterator.next()) |arg| {
-
-            self.mode = .edit;
-
-            self.console.print(arg);
-            self.console.print("\n");
-
             // parse argument
             const parsed_path = try ParsePath.init(arg);
-            
-            // open file
-            const file_name = parsed_path.file_name;
-            std.mem.copy(u8, self.file_name[0..], file_name); // copy file_name to global variable;
-            // DODO use zig api for file, but ONLY after zig release
-            const handle: *c.struct__IO_FILE = c.fopen(file_name.ptr, "rb") orelse return error.Unexpected; 
-            defer {
-                var fcloseResult = c.fclose(handle);
-                if(fcloseResult != 0) unreachable; // this is NOT unreachable, but zig not supports error in defer 0_o
-            }
-            
-            // read file size
-            _ = c.fseek(handle, 0, c.SEEK_END);
-            const size = @intCast(usize, c.ftell(handle));
-            const err_value = std.math.maxInt(u32);
-            if (size == err_value) return error.Unexpected;
+            std.mem.copy(u8, self.file_name[0..], parsed_path.file_name); // copy file_name to global variable;
 
-            // allock memory for file
-            // DODO rewrite this to zig allocator, but ONLY after zig release
-            const memory_ptr = c.malloc(size) orelse return error.Unexpected; 
-            const buffer = @ptrCast([*]u8, memory_ptr)[0..size];
-
-            // load full file to buffer.
-            _ = c.fseek(handle, 0, c.SEEK_SET);
-            const freadResult = c.fread(memory_ptr, 1, size, handle);
-            if(freadResult != size) return error.Unexpected;
-            self.console.print(buffer);
-
+            // load
+            const file_buffer = loadFile(parsed_path.file_name) catch |loadFile_result| switch (loadFile_result) {
+                error.FileNotExist => {
+                    return error.FileNotOpened; // TODO replace this to answer to create file.
+                },
+                error.Unexpected => return error.Unexpected,
+            };
+            self.mode = .edit;
+            self.console.print(file_buffer);
+            // TODO check - file map exist?
             // TODO create tab with this file
             // TODO parse this file
             // TODO if parsed_path.line goto line;
-
         }
     } // end else of if (std.os.argv.len == 1)
     self.mainLoop();
@@ -186,4 +163,33 @@ pub fn bufferClose() void {
     // TODO save file
 }
 
+pub fn loadFile(name: []const u8) error{
+    FileNotExist,
+    Unexpected,
+} ![]u8 {
+    // TODO check - file exits?
+    // open file DODO use zig api for file, but ONLY after zig release
+    const handle: *c.struct__IO_FILE = c.fopen(name.ptr, "rb") orelse return error.FileNotExist; 
+    defer {
+        var fcloseResult = c.fclose(handle);
+        if(fcloseResult != 0) unreachable; // this is NOT unreachable, but zig not supports error in defer 0_o
+    }
 
+    // read file size
+    _ = c.fseek(handle, 0, c.SEEK_END);
+    const size = @intCast(usize, c.ftell(handle));
+    const err_value = std.math.maxInt(u32);
+    if (size == err_value) return error.Unexpected;
+
+    // allock memory for file
+    // DODO rewrite this to zig allocator, but ONLY after zig release
+    const memory_ptr = c.malloc(size) orelse return error.Unexpected; 
+    const buffer = @ptrCast([*]u8, memory_ptr)[0..size]; // how to normal syntax to create slice?
+
+    // load full file to buffer.
+    _ = c.fseek(handle, 0, c.SEEK_SET);
+    const freadResult = c.fread(memory_ptr, 1, size, handle);
+    if(freadResult != size) return error.Unexpected;
+
+    return buffer;
+}
