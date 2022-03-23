443a17c5c4c431578d299a7af4e4f5e40e54eb63
Scalpi <sergey6661313@gmail.com>
2022-03-23 11:34:44 +0300

    todo

---
+++ src/Word.zig
@@ -0,0 +1,7 @@
+// logic while if else elif - siniy
+// skobki {} [] () = and - yellow
+// znaki + - * / - - red
+// chisla int or float or 0x - green
+// var int float local let - blue
+// local names - random 256 color
+ 
