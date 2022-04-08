739feb8812741ab008eae6db468fce7c1a0d4041
Scalpi <sergey6661313@gmail.com>
2022-04-08 19:25:06 +0300

    _

--- src/MapableFile.zig
+++ src/MapableFile.zig
@@ -1,7 +1,5 @@
 const Self = @This();
 const std  = @import("std");
-const MemAllocator = std.mem.Allocator;
-const HeapPageAllocator = std.heap.page_allocator;
 
 // fields
 file: std.fs.File = undefined,
@@ -11,6 +9,7 @@ pub fn fromRead(allocator: std.mem.Allocator, name: []const u8) !Self {
   var self: Self = .{};
   var cwd        = std.fs.cwd();
   self.file      = try cwd.openFile(name, .{.mode = .read_only});
+  defer self.file.close();
   const size     = try self.file.getEndPos();
   if (size == 0) return self;
   self.data      = try allocator.alloc(u8, size);
--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,9 @@ pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
-    var mapable_file = try MapableFile.fromRead(allocator, "src/main.zig");
+    var mapable_file = MapableFile.fromRead(allocator, "asd") catch |e| {
+      std.log.info("error = {}", .{e});
+      return;
+    };
     std.log.info("data = {s}", .{mapable_file.data.?});
 }
