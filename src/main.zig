c1b9074565de92a9dfdb202674de0533ad27ccf7
Scalpi <sergey6661313@gmail.com>
2022-04-08 12:18:02 +0300

    add arena deinit

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,10 @@
-const std        = @import("std");
+const std         = @import("std");
 const MapableFile = @import("MapableFile.zig");
 
 pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
     const allocator = arena.allocator();
     var mapable_file = try MapableFile.fromRead(allocator, "src/main.zig");
     std.log.info("data = {s}", .{mapable_file.data.?});
-}
-
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
 }
